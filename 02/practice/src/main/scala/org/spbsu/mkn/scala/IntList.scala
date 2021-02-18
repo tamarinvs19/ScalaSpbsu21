695e154533cde8c3b89921e5ee498606c0194727
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2021-02-18 18:42:08 +0300

    add IntList

--- 02/practice/src/main/scala/org/spbsu/mkn/scala/IntList.scala
+++ 02/practice/src/main/scala/org/spbsu/mkn/scala/IntList.scala
@@ -2,20 +2,52 @@ package org.spbsu.mkn.scala
 
 import org.spbsu.mkn.scala.IntList._
 
+
 sealed trait IntList {
   def head: Int
   def tail: IntList
-  def drop(n: Int): IntList
-  def take(n: Int): IntList
-  def map(f: Int => Int): IntList
-  def ::(elem: Int): IntList = ???
+  def drop(n: Int): IntList = n match {
+    case 0 => this
+    case _ => tail.drop(n - 1)
+  }
+  def take(n: Int): IntList = n match {
+    case 0 => IntNil
+    case _ => IntCons(head, tail.take(n - 1))
+  }
+  def map(f: Int => Int): IntList = IntCons(f(head), tail.map(f))
+  def ::(elem: Int): IntList = IntCons(elem, this)
+
+  def foldLeft(function: Int => Int => Int)(init: Int): Int = this match {
+    case IntNil => init
+    case _ => this.tail.foldLeft(function)(function(init)(head))
+  }
 }
+
+case object IntNil extends IntList {
+  override def head: Int = undef
+  override def tail: IntList = undef
+  override def map(f: Int => Int): IntList = IntNil
+}
+
+case class IntCons(head: Int, tail: IntList) extends IntList {}
 
 object IntList {
   def undef: Nothing = throw new UnsupportedOperationException("operation is undefined")
-  def fromSeq(seq: Seq[Int]): IntList = ???
-  def sum(intList: IntList): Int      = ???
-  def size(intList: IntList): Int     = ???
-  // extra task: implement sum using foldLeft
-  // def foldLeft(???)(???): ??? = ???
+
+  def fromSeq(seq: Seq[Int]): IntList = seq match {
+    case _ if seq.isEmpty => IntNil
+    case _                => IntCons(seq.head, fromSeq(seq.tail))
+  }
+
+  def sum(intList: IntList): Int      = intList match {
+    case IntNil => undef
+    case _      => intList.foldLeft(y => _ + y) (0)
+  }
+
+  def size(intList: IntList): Int     = intList match {
+    case IntNil => 0
+    case _      => 1 + size(intList.tail)
+  }
 }
+
+
