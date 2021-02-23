a66af87ecf01632c5c0dd33f1896ea9e19b38511
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2021-02-23 15:36:43 +0300

    Move realization, rewrite `size`

--- 02/practice/src/main/scala/org/spbsu/mkn/scala/IntList.scala
+++ 02/practice/src/main/scala/org/spbsu/mkn/scala/IntList.scala
@@ -6,30 +6,40 @@ import org.spbsu.mkn.scala.IntList._
 sealed trait IntList {
   def head: Int
   def tail: IntList
+  def drop(n: Int): IntList
+  def take(n: Int): IntList
+  def map(f: Int => Int): IntList
+  def ::(elem: Int): IntList
+  def foldLeft(function: Int => Int => Int)(init: Int): Int
+}
+
+case object IntNil extends IntList {
+  def head: Int = undef
+  def tail: IntList = undef
+  def drop(n: Int): IntList = undef
+  def take(n: Int): IntList = undef
+  def map(f: Int => Int): IntList = IntNil
+  def ::(elem: Int): IntList = IntCons(elem, this)
+  def foldLeft(function: Int => Int => Int)(init: Int): Int = init
+}
+
+case class IntCons(head: Int, tail: IntList) extends IntList {
   def drop(n: Int): IntList = n match {
     case 0 => this
     case _ => tail.drop(n - 1)
   }
+
   def take(n: Int): IntList = n match {
     case 0 => IntNil
     case _ => IntCons(head, tail.take(n - 1))
   }
+
   def map(f: Int => Int): IntList = IntCons(f(head), tail.map(f))
+
   def ::(elem: Int): IntList = IntCons(elem, this)
 
-  def foldLeft(function: Int => Int => Int)(init: Int): Int = this match {
-    case IntNil => init
-    case _ => this.tail.foldLeft(function)(function(init)(head))
-  }
+  def foldLeft(function: Int => Int => Int)(init: Int): Int = this.tail.foldLeft(function)(function(init)(head))
 }
-
-case object IntNil extends IntList {
-  override def head: Int = undef
-  override def tail: IntList = undef
-  override def map(f: Int => Int): IntList = IntNil
-}
-
-case class IntCons(head: Int, tail: IntList) extends IntList {}
 
 object IntList {
   def undef: Nothing = throw new UnsupportedOperationException("operation is undefined")
@@ -44,10 +54,7 @@ object IntList {
     case _      => intList.foldLeft(y => _ + y) (0)
   }
 
-  def size(intList: IntList): Int     = intList match {
-    case IntNil => 0
-    case _      => 1 + size(intList.tail)
-  }
+  def size(intList: IntList): Int     = intList.foldLeft(_ => _ + 1)(0)
 }
 
 
